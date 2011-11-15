6028109c4040762adb83737c31a74fe0b3dd0eef
Brendon Murphy <xternal1+github@gmail.com>
2011-11-14 19:08:32 -0800

    Fix up the spec helper to provide a test redis-server instance.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ def build_user!
   eval %Q{
       class User
         def id
-          @time ||= Time.now.to_f.to_s
+          @time ||= Time.now.to_f.to_s.tr('.', '').to_i
         end
 
         def self.where(*args)
@@ -43,6 +43,40 @@ build_user!
 
 require 'likeable'
 
+require 'tempfile'
 require 'rspec'
 require 'rspec/autorun'
 
+RSpec.configure do |config|
+  REDIS_PIDFILE = Tempfile.new("likeable-redis")
+
+  config.before(:suite) do
+    redis_options = {
+      "daemonize"     => 'yes',
+      "pidfile"       => REDIS_PIDFILE.path,
+      "bind"          => "127.0.0.1",
+      "port"          => 9737,
+      "timeout"       => 300,
+      "dir"           => "/tmp",
+      "loglevel"      => "debug",
+      "logfile"       => "stdout",
+      "databases"     => 16
+    }.map { |k, v| "#{k} #{v}" }.join('\n')
+    `echo '#{redis_options}' | redis-server -`
+
+    $redis_test_connection = Redis.new(:port => 9737, :db => 15)
+  end
+
+  config.before(:each) do
+    default_adapter!
+    $redis_test_connection.flushall
+    Redis.stub(:new).and_return($redis_test_connection)
+  end
+
+  # TODO this needs to handle premature exits, too
+  config.after(:suite) do
+    %x{
+      cat #{REDIS_PIDFILE.path} | xargs kill -QUIT
+    }
+  end
+end
